583aa883101f79cf3ba5e3727108f4b5e7be0a7c
ybuiko <juliabuiko@gmail.com>
2023-11-05 10:58:43 -0500

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -2,21 +2,40 @@
 // Easy Going
 //******************************
 //Write a for loop that will log the numbers 1 through 20.
+
 for (let i = 1; i <= 20; i++) {
   console.log(i);
 }
+
 //******************************
 // Get Even
 //******************************
 //Write a for loop that will log only the even numbers in 0 through 200.
+
 for (let i = 0; i <= 200; i += 2) {
   console.log(i);
 }
+
 //******************************
 // Fizz Buzz
 //******************************
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-//******************************
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 == 0) {
+    console.log("FizzBuzz", i);
+  } else if (i % 3 == 0) {
+    console.log("Fizz", i);
+  } else if (i % 5 == 0) {
+    console.log("Buzz", i);
+  }
+}
+
+// //******************************
 // Wild Wild Life
 //******************************
 
